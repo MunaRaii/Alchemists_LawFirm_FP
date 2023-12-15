5f8fde853c6266f3e9a237604e72ca666affc263
Muna Rai <144566608+MunaRaii@users.noreply.github.com>
2023-12-15 01:20:14 +0000

    js files are created with some data

---
+++ js/constants.js
@@ -0,0 +1,45 @@
+const defaultData = {
+    services: {
+        "uszsmadofre": {
+            id: "uszsmadofre",
+            imgUrl: "her is image url",
+            imgAltText: "image alt text",
+            title: "family violence case",
+            description: "Holistic family violence support: counseling, legal aid, and community resources. Empowering families for resilience and creating violence-free environments."
+        },
+        "gb2p0l5fx": {
+            id: "uszsmadofre",
+            imgUrl: "public company case",
+            imgAltText: "image alt text",
+            title: "public company case",
+            description: "Strategic solutions for public companies: governance, compliance, and growth strategies. Elevate your business with tailored services for sustained success."
+        },
+        "j8su5cfazo": {
+            id: "uszsmadofre",
+            imgUrl: "her is image url",
+            imgAltText: "image alt text",
+            title: "Business Tax Consultancy",
+            description: "Expert business tax consultancy: optimize returns, navigate regulations, and strategize for financial growth. Your partner in fiscal efficiency and success."
+        },
+        "cr9admku33s": {
+            id: "uszsmadofre",
+            imgUrl: "her is image url",
+            imgAltText: "image alt text",
+            title: "Marriage Agreement",
+            description: "Tailored Marriage Agreements: Expertly crafted legal documents ensuring clarity and protection, fostering a strong foundation for a lasting union."
+        },
+    },
+    users: {
+        "7z0r5o1t9dr": {
+            id: "7z0r5o1t9dr",
+            email: "munarai@gmail.com",
+            password: "munarai",
+            services: ["uszsmadofre"],
+            favServices: ["uszsmadofre"],
+            wishlistServices: ["gb2p0l5fx"]
+        }
+    },
+    activeUser: {
+
+    }
+};
---
+++ js/utils.js
@@ -0,0 +1,5 @@
+
+/* Generate the unique id */
+const generateID = ()=> {
+    return Math.random().toString(36).slice(2)
+}
