37bf3556892bbca4fd8699cd9f2a854798840bed
Muna Rai <144566608+MunaRaii@users.noreply.github.com>
2023-12-16 05:11:09 +0000

    create separate file for attach event

--- js/event.js
+++ js/event.js
@@ -1,7 +1,15 @@
 function attachServiceEvents() {
-
     /* Perform the add service function */
-    const addToServiceBtns = document.querySelectorAll('.' + className.addToService)
+    attachAddServiceEvent();
+
+    /* Perform the add to fav service function */
+    attachAddToFavServiceEvent();
+}
+
+
+function attachAddServiceEvent() {
+    const addToServiceBtns = document.querySelectorAll('.' + className.addToService);
+
     addToServiceBtns.forEach(btn => {
         btn.addEventListener('click', function (e) {
             e.preventDefault();
@@ -23,9 +31,10 @@ function attachServiceEvents() {
 
         })
     })
+}
 
 
-    /* Perform the add to fav service function */
+function attachAddToFavServiceEvent(){
     const addToFavServiceBtns = document.querySelectorAll('.' + className.addToFavService)
     addToFavServiceBtns.forEach(btn => {
         btn.addEventListener('click', function (e) {
@@ -43,7 +52,6 @@ function attachServiceEvents() {
                 this.classList.add('disabled');
                 this.text = BUTTON_TEXT.ADDED_TO_FAV;
             }
-
         })
     })
 }
